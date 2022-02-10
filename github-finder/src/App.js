69538d481445e81345b3b75ca167fc8438e7adcf
Georgi <georgi.nenov89@gmail.com>
2022-02-10 10:33:11 -0500

    ain page flexbox ready

---
+++ github-finder/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Navbar from './components/layout/Navbar';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
+
+          <main>
+            Content here
+          </main>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
